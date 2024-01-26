a178cfdffc145a28f81b418f4ddb3dd564fc798c
Çağan <cagangdk6@gmail.com>
2024-01-27 01:53:43 +0300

    Create local_ip_changer.py

---
+++ local_ip_changer.py
@@ -0,0 +1,21 @@
+import os
+import pyfiglet 
+import random 
+import time
+f = pyfiglet.figlet_format("Local ip changer", font="slant")
+print(f)
+
+
+second=int(input("[+]How many second do you want to between two ip for change:"))
+interface=input("[+]What type of interface do you use (eth0, wlan0):")
+	
+try:
+	while True:
+		random_ip='192.168.'+str(random.randint(0,256))+"."+str(random.randint(0,256))
+		time.sleep(second)
+		os.system('ifconfig '+interface+" "+random_ip)
+		print('[+]Your ip address changed as '+random_ip)
+except KeyboardInterrupt:
+	os.system("service networking restart")
+	os.system("service NetworkManager restart")
+	print("\n[+]networking and NetworkManager services has been restarted")
